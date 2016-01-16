dc2aac625443d47f565af98f0ea55d3200b001ea
xADMx <adm589@mail.ru>
2016-01-16 12:55:43 +0300

    добавление функций шифрования

--- index2.php
+++ index2.php
@@ -1,6 +1,14 @@
 <?php
 
-	private function definition_mac($ipAddress) {
+		$r = fopen("../key/public.pem",'r');
+		$Publickkey = fread($r,filesize("../key/public.pem"));
+		openssl_get_publickey($Publickkey);
+
+		$r = fopen("../key/private.pem",'r');
+		$PrivateKey = fread($r,filesize("../key/private.pem"));
+
+
+	function definition_mac($ipAddress) {
 
 		 if(PHP_OS == 'Linux'){  $macAddr = exec("grep ".$ipAddress." /proc/net/arp | awk '{print $4}'");  }
 		 elseif(PHP_OS == 'WINNT'){
@@ -20,15 +28,18 @@
 	//Strat
 	$GetParam = isset($_GET['one']) ? stripslashes($_GET['one']) : false;
 	if (!$GetParam) {
-		$Param = json_decode(file_get_contents('http://wifi.free-ddyns.ru/secret/' . definition_mac($_SERVER["REMOTE_ADDR"])), true);
-		header( 'Location: http://www.example.com/'.$Param['rediect'], true, 301 );
+		//$Param = json_decode(file_get_contents('http://wifi.free-ddyns.ru/secret/' . definition_mac($_SERVER["REMOTE_ADDR"])), true);
+		//header( 'Location: http://www.example.com/'.$Param['rediect'], true, 301 );
+		$mac = definition_mac($_SERVER["REMOTE_ADDR"]);
+		echo $mac;
+
+		openssl_public_encrypt($mac,$crypttext,$Publickkey);
+		echo "<br>Mac : ".$crypttext;
+
+		openssl_private_decrypt($crypttext,$newsource,$PrivateKey);
+		echo "<br>Mac : ".$newsource;
+
     } else {
-    	$Param = json_decode(file_get_contents('http://wifi.free-ddyns.ru/token/' . $GetParam), true);    }
-
-                    //$user['network'] - соц. сеть, через которую авторизовался пользователь
-                    //$user['identity'] - уникальная строка определяющая конкретного пользователя соц. сети
-                    //$user['first_name'] - имя пользователя
-                    //$user['last_name'] - фамилия пользователя
-
-
+    	$Param = json_decode(file_get_contents('http://wifi.free-ddyns.ru/token/' . $GetParam), true);
+    }
 ?>
